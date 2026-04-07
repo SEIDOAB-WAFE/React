0910573682fbf8b70de856781993d5c413638dc3
Martin <martin_lenart@icloud.com>
2026-04-07 14:58:33 +0200

    update

--- m4l01/src/lesson-steps/04-dataflow/components/09-nested-components.js
+++ m4l01/src/lesson-steps/04-dataflow/components/09-nested-components.js
@@ -2,9 +2,18 @@ import React from 'react'
 import { uniqueId, seedGenerator } from '../../../services/seido-helpers';
 
 export function NestedComponent(props) {
+  
+  const clickHandler = (e) => {
+    console.log("Hello from NestedComponent")
+
+    if (props.onClick) {
+      props.onClick(e)
+    }
+  }
+  
   return (
     <>
-    <div>hello world says: {props.greeting}</div>
+    <div onClick={clickHandler}>hello world says: {props.greeting}</div>
     </>
   )
 }
@@ -12,10 +21,15 @@ export function NestedComponent(props) {
 
 export function WrapperComponent(props) {
 
+  const clickHandler = (e) => {
+    console.log("Hello from WrapperComponent")
+  }
+
   //use React.Children.map to add props to children of a component
   //use React.cloneElement to clone the child and add props to it
   const nestedChildren = React.Children.map(props.children, child =>
-    React.cloneElement(child, { greeting: props.greeting }));
+    React.isValidElement(child) ? React.cloneElement(child, {}) : child);
+
 
   return (
     <>
